5c3c5d1ba0a37c7c3d4933be30ff07734448ab0c
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-08-11 21:56:19 -0500

    Experiment on keyframe.

---
+++ keyframe/src/components/hello/Hello.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./hello.css";
+
+//import "./App.css";
+
+class HelloContainer extends React.Component {
+
+    render() {
+        return (
+            <div id="cuadro">
+                <h1>Hello CodeSandbox</h1>
+                <h2>Start editing to see some magic happen! :)</h2>
+            </div>
+        );
+    }
+}
+
+export default HelloContainer;
